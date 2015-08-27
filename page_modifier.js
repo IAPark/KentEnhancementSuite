5e8f36bc8b76b18ad98c30eefe83cf6777868fc0
Isaac Park <isaac.a.park@gmail.com>
2015-08-27 17:37:53 -0400

    Adds Support For Other Campuses

--- page_modifier.js
+++ page_modifier.js
@@ -7,21 +7,21 @@ $(function () {
     rows.each(function (index, row) {
         if (index % 2 != 0) {
             console.log(row);
-            var crn = $(row).find("td")[1].innerHTML;
-            var course = crn.split('-');
-            console.log(course);
-            console.log(crn);
+            var boxes = $(row).find("td");
+            var course_text = boxes[1].innerHTML;
+            var course = course_text.split('-');
+            var campus = boxes[3].innerHTML;
             var department = course[0];
             var course_num = parseInt(course[1]);
             var section = parseInt(course[2]);
             var term = 201580; // wish I knew why
-            $(row).prepend("<td class=\"dddefault\" style=\"border:tan solid 1px;\">\n                    <a href=\"" + get_book_search_url(course_num, department, section, term) + "\" target=\"_blank\">\n                        Book\n                    </a>\n                </td>");
+            $(row).prepend("<td class=\"dddefault\" style=\"border:tan solid 1px;\">\n                    <a href=\"" + get_book_search_url(campus, course_num, department, section, term) + "\" target=\"_blank\">\n                        Book\n                    </a>\n                </td>");
         }
     });
 });
-function get_book_search_url(course_num, department, section, term) {
+function get_book_search_url(campus, course_num, department, section, term) {
     var base_url = "https://securex.bncollege.com/" + "webapp/wcs/stores/servlet/TBListView?cm_mmc=RI-_-8152-_-1-_-A&catalogId=10001&storeId=87857&langId=-1&%20" + "termMapping=Y&courseXml=";
-    var xml = '<?xml version="1.0" encoding="UTF-8"?>' + '<textbookorder>' + '<campus name="KC">' + '<courses>' + '<course num="' + course_num + '" dept="' + department + '" sect="' + ("00" + section).slice(-3) + '" term="' + term + '"/>' + '</courses>' + '</campus>' + '</textbookorder>';
+    var xml = '<?xml version="1.0" encoding="UTF-8"?>' + '<textbookorder>' + '<campus name="' + campus + '">' + '<courses>' + '<course num="' + course_num + '" dept="' + department + '" sect="' + ("00" + section).slice(-3) + '" term="' + term + '"/>' + '</courses>' + '</campus>' + '</textbookorder>';
     return base_url + encodeURIComponent(xml);
 }
 //# sourceMappingURL=page_modifier.js.map
--- page_modifier.ts
+++ page_modifier.ts
@@ -8,18 +8,18 @@ $(() => {
     rows.each((index, row) => {
         if(index%2 != 0) {
             console.log(row);
-            var crn = $(row).find("td")[1].innerHTML;
-            var course = crn.split('-');
-            console.log(course);
-            console.log(crn);
+            var boxes = $(row).find("td");
+            var course_text = boxes[1].innerHTML;
+            var course = course_text.split('-');
 
+            var campus = boxes[3].innerHTML;
             var department = course[0];
             var course_num = parseInt(course[1]);
             var section = parseInt(course[2]);
             var term = 201580; // wish I knew why
             $(row).prepend(
                 `<td class="dddefault" style="border:tan solid 1px;">
-                    <a href="`+get_book_search_url(course_num, department, section, term)+`" target="_blank">
+                    <a href="`+get_book_search_url(campus, course_num, department, section, term)+`" target="_blank">
                         Book
                     </a>
                 </td>`
@@ -28,14 +28,14 @@ $(() => {
     });
 });
 
-function get_book_search_url(course_num: number, department: string, section: number, term: number):string {
+function get_book_search_url(campus: string, course_num: number, department: string, section: number, term: number):string {
     var base_url = "https://securex.bncollege.com/" +
         "webapp/wcs/stores/servlet/TBListView?cm_mmc=RI-_-8152-_-1-_-A&catalogId=10001&storeId=87857&langId=-1&%20" +
         "termMapping=Y&courseXml=";
     var xml =
         '<?xml version="1.0" encoding="UTF-8"?>' +
         '<textbookorder>' +
-        '<campus name="KC">' +
+        '<campus name="'+campus+'">' +
         '<courses>' +
         '<course num="'+course_num+'" dept="'+department+'" sect="'+("00"+section).slice(-3)+'" term="'+term+'"/>' +
         '</courses>' +
